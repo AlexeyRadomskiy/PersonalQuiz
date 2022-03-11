85f418f8e7f2ff7cc507f0284303fbb9d4e3b017
Alexey <alexeyradomskiy98@gmail.com>
2022-03-11 20:44:51 +0300

    Setup Result View Controller

--- PersonalQuizz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuizz/ViewControllers/QuestionsViewController.swift
@@ -42,7 +42,8 @@ class QuestionsViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        guard let resultVC = segue.destination as? ResultViewController else { return }
+        resultVC.answer = answersChosen
     }
     
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
--- PersonalQuizz/ViewControllers/ResultViewController.swift
+++ PersonalQuizz/ViewControllers/ResultViewController.swift
@@ -9,4 +9,38 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var answer: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        updateResult()
+    }
+    
+    private func updateResult() {
+        
+        var repeatAnimals: [Animal: Int] = [:]
+        let animals = answer.map { $0.animal }
+        
+        for animal in animals {
+            if let animalTypeCount = repeatAnimals[animal] {
+                repeatAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                repeatAnimals[animal] = 1
+            }
+        }
+        
+        let sortedRepeatAnimals = repeatAnimals.sorted { $0.value > $1.value }
+        guard let mostRepeatAnimals = sortedRepeatAnimals.first?.key else { return }
+        
+        updateUI(with: mostRepeatAnimals)
+    }
+    
+    private func updateUI(with animal: Animal) {
+        resultLabel.text = "Вы - \(animal.rawValue)!"
+        descriptionLabel.text = animal.definition
+    }
 }
